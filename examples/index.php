b632ecabc28bfe26fd77f76422715001f7b5271a
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2012-08-16 19:08:57 +0200

    Changed variable names, eliminated load function, various comments.

--- examples/index.php
+++ examples/index.php
@@ -11,10 +11,10 @@ phex::set('TEST.ARRAY.VAR.VAR2', "valore");
 phex::set('NOARRAY', "valore");
 
 phex::route("/", function(){echo "OK";});
+//phex::route("/", "example::test");
 phex::route("/@test1/ciao/test2", 'test');
 phex::route("/@test1/@plutone/@test2", 'test');
 phex::route("/@test1/ciao/@test2", 'test');
 phex::run();
-echo $_PHEX['TEST']['ARRAY']['VAR']['VAR2'];
 echo phex::get('TEST.ARRAY.VAR.VAR2');
 ?>
--- lib/phex.php
+++ lib/phex.php
@@ -1,92 +1,82 @@
 <?php
 
+/**
+	ph.ex Framework
+	
+	Copyright (c) 2012
+	Vincenzo Petrucci (vincenzo.petrucci@gmail.com)
+	http://www.vincenzopetrucci.it/ph.ex/
+	https://github.com/nahime/ph.ex
+	
+		@version 0.0.1
+**/
+
 class phex
 {
-	
-	private static $VERSION = "0.0.1";
-	private static $ACCEPTED_METHODS = array("GET", "POST");
-	private static $_PHEX = array();
-	private static $_ROUTES;
-	private static $loaded = false;
-	
-	
-	function load()
-	{
-		if(!self::$loaded)
-		{
-			self::$_PHEX['VERSION'] = self::$VERSION;
-			self::$_PHEX['AUTOLOAD'] = "autoload/";
-			self::$loaded = true;
-		}
-	}
-	
+	private static $P = array();
+
 	/**
 		Provides isMETHOD and routeMETHOD.
 		METHOD could be one of the defined http ACCEPTED_METHODS
 	**/
 	static function __callStatic($method, $arguments) {
-		if(substr($method,0,2) == "is" && 
+		if(substr($method,0,2) == "is" &&
 			in_array(
-				substr($method,2,strlen($method)), 
-				self::$ACCEPTED_METHODS))
+				substr($method,2,strlen($method)),
+				self::$P['ACCEPTED_METHODS']))
 		{
 			$http_method = substr($method,2,strlen($method));
 			return $http_method == $_SERVER['REQUEST_METHOD'] ? true : false;
 		}
-		elseif(substr($method,0,5) == "route" && 
+		elseif(substr($method,0,5) == "route" &&
 			in_array(
-				substr($method,5,strlen($method)), 
-				self::$ACCEPTED_METHODS))
+				substr($method,5,strlen($method)),
+				self::$P['ACCEPTED_METHODS']))
 		{
 			$http_method = substr($method,5,strlen($method));
 			if ($http_method == $_SERVER['REQUEST_METHOD'])
 			{
 				self::route($arguments[0], $arguments[1]);
 			}
-		} 
+		}
 	}
-	
+
 	/**
-	
+		Define a route
+			@param $route string
+			@param $callback mixed
 	**/
-	static function route($req_route, $callback)
+	static function route($route, $callback)
 	{
-		if(isset(self::$_ROUTES[0]) && isset(self::$_ROUTES[0]['CALLBACK']))
+		if(isset(self::$P['ROUTES'][0]) && isset(self::$P['ROUTES'][0]['CALLBACK']))
 		{
 			/*
 			* Still exists an exact match route, we can skip all others.
 			*/
 			return;
 		}
-
-		if(strpos($req_route, "@") === false)
+		if(strpos($route, "@") === false)
 		{
-			if($_SERVER['REQUEST_URI'] !== $req_route)
+			if($_SERVER['REQUEST_URI'] == $route)
 			{
-				//echo "INCOMPATIBLE_A";
-				return;
-			}
-			else
-			{
-				self::$_ROUTES[0]['CALLBACK'] = $callback;
-				self::$_ROUTES[0]['PARAMS'] = null;
+				self::$P['ROUTES'][0]['CALLBACK'] = $callback;
+				self::$P['ROUTES'][0]['PARAMS'] = null;
 			}
 		}
 		else
 		{
-			$req_route_data = explode("/", trim($req_route, '/'));
-			$req_uri_data = explode("/", trim($_SERVER['REQUEST_URI'], '/'));
-			$num_parameters = substr_count($req_route, "@"); // Check that there are no other routes with this number of parameters
-			if (sizeof($req_route_data) == sizeof($req_uri_data))
+			$route_data = explode("/", trim($route, '/'));
+			$uri_data = explode("/", trim($_SERVER['REQUEST_URI'], '/'));
+			$n_pars = substr_count($route, "@");
+			if (sizeof($route_data) == sizeof($uri_data))
 			{
 				$candidate_route = array();
-				for($i = 0; $i < sizeof($req_route_data); $i++)
+				for($i = 0; $i < sizeof($route_data); $i++)
 				{
-					$token_route = $req_route_data[$i];
-					$token_uri = $req_uri_data[$i];
+					$token_route = $route_data[$i];
+					$token_uri = $uri_data[$i];
 					if(substr($token_route,0,1) !== "@" && $token_route !== $token_uri)
 					{
-						//echo "INCOMPATIBLE_B";
 						return;
 					}
 					elseif(substr($token_route,0,1) == "@")
@@ -94,32 +84,32 @@ class phex
 						$candidate_route[substr($token_route,1,strlen($token_route))] = $token_uri;
 					}
 				}
-				self::$_ROUTES[$num_parameters]['PARAMS'] = $candidate_route; // Should save also some other data
-				self::$_ROUTES[$num_parameters]['CALLBACK'] = $callback;
-			}
-			else
-			{
-				//echo "INCOMPATIBLE_C";
-				return;
+				self::$P['ROUTES'][$n_pars]['PARAMS'] = $candidate_route; // Should save also some other data
+				self::$P['ROUTES'][$n_pars]['CALLBACK'] = $callback;
 			}
 		}
 	}
 	
 	/**
-	
+		Redirects the user
+			@param $loc string
+	**/
+	static function reroute($loc)
+	{
+		header("Location: ".$loc);
+		exit();
+	}
+
+	/**
+		Starts the system
 	**/
 	static function run()
 	{
-		self::load();
-		global $_PHEX;
-		$_PHEX = self::$_PHEX;
-		
-		if(sizeof(self::$_ROUTES) == 0)
+		if(sizeof(self::$P['ROUTES']) == 0)
 		{
 			self::error(404);
 		}
-			
-		$autoloads = explode(",", self::$_PHEX['AUTOLOAD']);
+		$autoloads = explode(",", self::$P['AUTOLOAD']);
 		$al_paths = array();
 		foreach($autoloads as $autoload)
 		{
@@ -131,15 +121,13 @@ class phex
 		}
 		if(sizeof($al_paths) > 0)
 		{
-			set_include_path(get_include_path().PATH_SEPARATOR.implode(PATH_SEPARATOR, $al_paths));	
+			set_include_path(get_include_path().PATH_SEPARATOR.implode(PATH_SEPARATOR, $al_paths));
 		}
 		spl_autoload_extensions(".php");
 		spl_autoload_register();
-		
 		ob_start();
-		ksort(self::$_ROUTES);
-		$_PHEX['ROUTES'] = self::$_ROUTES;
-		foreach(self::$_ROUTES as $route)
+		ksort(self::$P['ROUTES']);
+		foreach(self::$P['ROUTES'] as $route)
 		{
 			$callbacks = $route['CALLBACK'];
 			if(is_object($callbacks) && ($callbacks instanceof Closure))
@@ -150,7 +138,6 @@ class phex
 			{
 				$callbacks = explode(";", $callbacks);
 			}
-			
 			if(is_array($callbacks))
 			{
 				foreach($callbacks as $callback)
@@ -161,7 +148,7 @@ class phex
 					}
 					elseif(is_callable($callback))
 					{
-						eval($callback."();");	
+						eval($callback."();");
 					}
 					elseif(strpos($callback, "->") > 0)
 					{
@@ -185,7 +172,7 @@ class phex
 								{
 									throw new Exception('Cannot load class: '.$callclass);
 								}
-								$var = new $callclass();					
+								$var = new $callclass();
 							}
 							$var->$callmethod();
 						}
@@ -199,40 +186,40 @@ class phex
 						trigger_error("Invalid callback");
 					}
 				}
-			} // if(is_array($callbacks))
-			
-			$_PHEX['ROUTE'] = $route;
+			} /* if(is_array($callbacks)) */
+			$P['ROUTE'] = $route;
 			break;
-		} //  foreach(self::$_ROUTES as $route)
+		} /* foreach(self::$P['ROUTES] as $route) */
 		$response = ob_get_clean();
 		echo $response;
 	}
-	
+
 	/**
 		Retrieve saved var
-			@param item string
+			@param $item string
 	**/
 	static function get($item)
 	{
 		return eval('
-			return isset(self::$_PHEX[\''.str_replace('.','\'][\'', $item).'\']) ? 
-				self::$_PHEX[\''.str_replace('.','\'][\'', $item).'\'] :
+			return isset(self::$P[\''.str_replace('.','\'][\'', $item).'\']) ?
+				self::$P[\''.str_replace('.','\'][\'', $item).'\'] :
 				null;
 		');
-		if (isset(self::$_PHEX[$item]))
+		if (isset(self::$P[$item]))
 		{
-			return self::$_PHEX[$item];
+			return self::$P[$item];
 		}
 		else
 		{
 			return null;
 		}
 	}
-	
+
 	/**
 		Set var
-			@param item string
-			@param value mixed
+			@param $item string
+			@param $value mixed
+			@param $recursive boolean
 	**/
 	static function set($item, $value, $recursive = false)
 	{
@@ -241,18 +228,19 @@ class phex
 			$item = explode('.', $item, 2);
 			$thisitem = $item[0];
 			$nextitem = $item[1];
-			return $recursive ? 
+			return $recursive ?
 				array($thisitem => self::set($nextitem, $value, true)) :
-				self::$_PHEX[$thisitem] = self::set($nextitem, $value, true);
+				self::$P[$thisitem] = self::set($nextitem, $value, true);
 		}
 		else
 		{
-			return $recursive ? array($item => $value) : (self::$_PHEX[$item] = $value);
+			return $recursive ? array($item => $value) : (self::$P[$item] = $value);
 		}
 	}
-	
+
 	/**
-	
+		Show an error
+			@param $code integer
 	**/
 	static function error($code)
 	{
@@ -268,4 +256,11 @@ class phex
 		exit();
 	}
 }
+
+/**
+	Default configuration
+**/
+phex::set('AUTOLOAD', 'autoload/');
+phex::set('VERSION', '0.0.1');
+phex::set('ACCEPTED_METHODS', array('GET', 'POST'));
 ?>
